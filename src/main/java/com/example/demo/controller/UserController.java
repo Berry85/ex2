563888d259de63900ee1a340a2ef45d1e4705377
Berry85 <1182346874@qq.com>
2019-12-14 04:25:51 +0800

    Wrong

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.User;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
+@Controller
+@ComponentScan(basePackages={"com.example.demo.controller", "resources.static"})
 public class UserController {
 	@RequestMapping("/register")
 	public String register(@ModelAttribute("form") User user, Model model)
