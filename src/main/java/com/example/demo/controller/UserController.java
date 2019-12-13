3dcac4eeb2a09990a049ca92e4099a9b9bc2e5b9
Berry85 <1182346874@qq.com>
2019-12-14 04:42:27 +0800

    总算成功了

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
@@ -9,13 +11,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@SpringBootApplication(scanBasePackages = "com.example.demo.controller")
 @Controller
-@ComponentScan(basePackages={"com.example.demo.controller", "resources.static"})
 public class UserController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserController.class, args);
+	}
+
+
 	@RequestMapping("/register")
 	public String register(@ModelAttribute("form") User user, Model model)
 	{
 		model.addAttribute(user);
-		return "success";
+		return "/success.html";
 	}
 }
